80e0db493ddf37672e77cea6296feb9ad2cb4579
Théo Kimbangui <theokimbangui36@gmail.com>
2024-08-02 21:38:34 +0200

    feat: Contact animation

---
+++ src/app/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import Image from 'next/image';
+import { useState, ReactNode } from 'react';
+
+export default function Home() {
+  const [hovered, setHovered] = useState<HoveredItem>(HoveredItem.None);
+
+  const handleHover = (hover: HoveredItem) => {
+    setHovered(hover);
+  };
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="relative shadow-custom px-10 lg:px-16 py-24 rounded-xl">
+        <p className="relative flex text-3xl font-bold" id="email">
+          <div className="relative flex">
+            <HoverableSpan hoverType={HoveredItem.Email} hovered={hovered}>
+              <HoverableSpan hoverType={HoveredItem.Name} hovered={hovered}>
+                theo
+              </HoverableSpan>
+              @
+              <HoverableSpan hoverType={HoveredItem.Github} hovered={hovered}>
+                mb1mba
+              </HoverableSpan>
+              .com
+            </HoverableSpan>
+          </div>
+        </p>
+        <ul className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-2">
+          <HoverableIcon src="/user.png" alt="User icon" hoverType={HoveredItem.Name} handleHover={handleHover} />
+          <HoverableIcon src="/email.png" alt="Email icon" hoverType={HoveredItem.Email} handleHover={handleHover} />
+          <HoverableIcon src="/github.png" alt="Github icon" hoverType={HoveredItem.Github} handleHover={handleHover} />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+enum HoveredItem {
+  Name = "name",
+  Email = "email",
+  Github = "github",
+  None = ""
+}
+
+interface HoverableSpanProps {
+  hoverType: HoveredItem;
+  hovered: HoveredItem;
+  children: ReactNode;
+}
+
+const HoverableSpan: React.FC<HoverableSpanProps> = ({ hoverType, hovered, children }) => {
+  const isHovered = hovered === hoverType;
+  const shouldFade = hovered === HoveredItem.Github || hovered === HoveredItem.Name;
+
+  return (
+    <span className={`relative ${isHovered ? 'before:opacity-100 before:-bottom-1 text-black' : 'before:opacity-0 before:bottom-2'} before:content-['${hoverType.charAt(0).toUpperCase() + hoverType.slice(1)}'] before:pt-2 before:font-normal before:text-xs before:text-center before:absolute before:h-2 before:w-full before:border-b-2 before:border-l-2 before:border-r-2 before:border-dashed before:transition-all before:duration-200 before:ease-out`}>
+      <span className={`${isHovered ? "text-violet-600 transition-colors ease-out duration-300" : shouldFade ? "text-gray-300 transition-colors ease-out duration-300" : "transition-colors ease-out duration-200"}`}>
+        {children}
+      </span>
+    </span>
+  );
+};
+
+interface HoverableIconProps {
+  src: string;
+  alt: string;
+  hoverType: HoveredItem;
+  handleHover: (hover: HoveredItem) => void;
+}
+
+const HoverableIcon: React.FC<HoverableIconProps> = ({ src, alt, hoverType, handleHover }) => {
+  return (
+    <li
+      onMouseEnter={() => handleHover(hoverType)}
+      onMouseLeave={() => handleHover(HoveredItem.None)}
+      className="cursor-pointer hover:-translate-y-2 transition-tranform duration-150 ease-linear"
+    >
+      <Image
+        src={src}
+        width={25}
+        height={25}
+        alt={alt}
+      />
+    </li>
+  );
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow:
+      {
+        "custom": "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px"
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
